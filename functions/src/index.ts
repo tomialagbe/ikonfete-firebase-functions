32fabf7525c070f650943c8808ab616027b31875
Tomi Alagbe <tomialagbe>
2019-03-21 00:17:28 +0100

    Added verify artist function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,7 +5,7 @@ import * as handlebars from 'handlebars';
 import * as moment from 'moment';
 import * as nodemailer from 'nodemailer';
 import * as templates from './templates';
-import { DocumentReference } from '@google-cloud/firestore';
+import { DocumentReference, CollectionReference, QueryDocumentSnapshot } from '@google-cloud/firestore';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -23,9 +23,9 @@ export const sendArtistActivationCode = functions.firestore.document("artists/{u
         const newUser = snapshot.data();
         if (newUser) {
             const email = newUser.email;
-            const name = newUser.name;    
+            const name = newUser.name;
             const uid = newUser.uid;
-            await _sendActivationCode(uid, name, email, true);            
+            await _sendActivationCode(uid, name, email, true);
         }
         return snapshot;
     });
@@ -35,7 +35,7 @@ export const sendFanActivationCode = functions.firestore.document("fans/{userId}
         const newUser = snapshot.data();
         if (newUser) {
             const email = newUser.email;
-            const name = newUser.name;    
+            const name = newUser.name;
             const uid = newUser.uid;
             await _sendActivationCode(uid, name, email, false);
         }
@@ -46,12 +46,12 @@ export const sendFanActivationCode = functions.firestore.document("fans/{userId}
 export const deleteFirebaseUser = functions.https.onCall(async (data, context) => {
     const uid = data.uid;
     const firebaseAuth = admin.auth();
-    try {    
+    try {
         await firebaseAuth.deleteUser(uid);
-        return {success: true};
+        return { success: true };
     } catch (err) {
         console.log(`Failed to delete user. ${err}`);
-        return {success: false, error: `Failed to delete user ${uid}. ${err}`};
+        return { success: false, error: `Failed to delete user ${uid}. ${err}` };
     }
 });
 
@@ -67,24 +67,24 @@ export const resendActivationCode = functions.https.onCall(async (req, context) 
         // find the activation for this uid        
         const firestore = admin.firestore();
         let querySnapshot = await firestore.collection("activations").where("uid", "==", uid).limit(1).get();
-        const oldActivation = querySnapshot.docs[0];        
-        const docData: FirebaseFirestore.DocumentData = oldActivation.data();        
+        const oldActivation = querySnapshot.docs[0];
+        const docData: FirebaseFirestore.DocumentData = oldActivation.data();
         await oldActivation.ref.delete();
         const isArtist = docData["isArtist"];
         uid = docData["uid"];
-        const collectionName = isArtist ? "artists" : "fans"; 
+        const collectionName = isArtist ? "artists" : "fans";
         querySnapshot = await firestore.collection(collectionName).where("uid", "==", uid).limit(1).get();
         const user: FirebaseFirestore.DocumentData | null = querySnapshot.docs.length > 0 ? querySnapshot.docs[0].data() : null;
         if (user === null) {
-            return {success: false, error: isArtist ? "Artist " : "Fan " + "not found"};
+            return { success: false, error: isArtist ? "Artist " : "Fan " + "not found" };
         }
 
         await _sendActivationCode(uid, user.name, user.email, isArtist);
         console.log(`Resent activation to ${user.email}\n`)
-        return {success: true, error: ""};
+        return { success: true, error: "" };
     } catch (err) {
         console.log(`Failed to resend activation: ${err}`);
-        return {success: false, error: "Failed to resend activation: " + err};
+        return { success: false, error: "Failed to resend activation: " + err };
     }
 });
 
@@ -98,11 +98,44 @@ export const activateUser = functions.https.onCall(async (data, context) => {
 
     try {
         const userRecord: admin.auth.UserRecord = await auth.updateUser(uid, updateRequest);
-        return {success: true, uid: userRecord.uid};
+        return { success: true, uid: userRecord.uid };
     } catch (error) {
         const errMessage = `Failed to update firebase user: ${error}`;
         console.log();
-        return {success: false, error: errMessage};
+        return { success: false, error: errMessage };
+    }
+});
+
+export const verifyArtist = functions.https.onCall(async (data, context) => {
+    try {
+        const uid = data.uid;
+        // const auth = admin.auth();
+        const firestore = admin.firestore();
+        const artistCollection: CollectionReference = firestore.collection("artists");
+        let querySnapshot = await artistCollection.where("uid", "==", uid).limit(1).get()
+        if (querySnapshot.docs.length == 0) {
+            return { success: false, error: "Artist not found" };
+        }
+
+        const doc: QueryDocumentSnapshot = querySnapshot.docs[0];
+        const artist = doc.data();
+        artist["isVerified"] = true;
+        artist["isPendingVerification"] = false;
+        artist["dateVerified"] = moment().utc().valueOf();
+        artist["dateUpdated"] = moment().utc().valueOf();
+        const result = await doc.ref.update(artist);
+        console.log(`Successfully updated user ${uid} at ${result.writeTime.toMillis()}.`);
+
+        // delete pending verification
+        const pvCollecction: CollectionReference = firestore.collection("pending_verifications");
+        querySnapshot = await pvCollecction.where("uid", "==", uid).get();
+        querySnapshot.forEach(async (snap) => {
+            await snap.ref.delete();
+        });
+        return { success: true };
+    } catch (error) {
+        console.log(`Failed to verify user. ${error}`);
+        return { success: false, error: error };
     }
 });
 
@@ -126,35 +159,35 @@ async function _sendActivationCode(uid: string, name: string, email: string, isA
         console.log(`Error sending mail: ${err}\n`);
         return false;
     }
-    
+
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
             user: 'tommytee30@gmail.com',
             pass: 'two4one3632'
-        },                
+        },
     });
-        
-    const mailOptions : nodemailer.SendMailOptions = {
-        from: 'tommytee30@gmail.com',                
-        to: email,                
+
+    const mailOptions: nodemailer.SendMailOptions = {
+        from: 'tommytee30@gmail.com',
+        to: email,
         subject: 'Your Ikonfete Activation Code',
         html: activationTemplate({
             "name": name, "code": code,
         }),
     };
-    
-    try {        
+
+    try {
         await transporter.sendMail(mailOptions);
         console.log(`Email sent to ${email}\n`);
     } catch (error) {
         console.log(`Error sending mail: ${error}\n`);
         return false;
-    }    
+    }
 
-    return true;          
+    return true;
 }
-    
+
 function randomString(length: number) {
     const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let result = '';
