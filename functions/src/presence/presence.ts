31336b794b08850517037d771236a281b384aaea
Tomi Alagbe <tomialagbe>
2019-04-26 15:34:22 +0100

    Modified presence
    Modified presence to use user model instead of separate model

--- functions/src/presence/presence.ts
+++ functions/src/presence/presence.ts
@@ -3,40 +3,46 @@ import { Firestore, CollectionReference, QueryDocumentSnapshot, WriteResult } fr
 import moment = require('moment');
 // import { firestore } from 'firebase-admin';
 
-interface Pair<F, S>{
+interface Pair<F, S> {
     first: F;
     second: S;
 }
 
-export const onUserStatusChanged = functions.database.ref("/status/{userId}")
-    .onUpdate((change, ctx) => {
-        console.log("Update recieved");
-        if (ctx.auth === undefined || change.after === undefined || !change.after.exists()) {
-            return false;
-        }
-        const uid = ctx.auth.uid;
-        const status = change.after.val();
+export const onArtistStatusChanged = functions.database.ref("/status/artists/{userId}")
+    .onUpdate((change, ctx) => _handleUserStatusChanged(change, ctx, true));
 
-        console.log("Handling update");
+export const onFanStatusChanged = functions.database.ref("/status/fans/{userId}")
+    .onUpdate((change, ctx) => _handleUserStatusChanged(change, ctx, false));
 
-        const firestore = new Firestore();
-        const ref: CollectionReference = firestore.collection("user_presence");
-        if (status === 'offline') {
-            ref.where("uid", "==", uid).limit(1).get().then((querySnapshot) => {
+function _handleUserStatusChanged(change: functions.Change<functions.database.DataSnapshot>, ctx: functions.EventContext, isArtist: boolean): boolean {
+    console.log("Update recieved");
+    if (ctx.auth === undefined || change.after === undefined || !change.after.exists()) {
+        return false;
+    }
+    const uid = ctx.auth.uid;
+    const status = change.after.val();
+
+    console.log("Handling update");
+
+    const firestore = new Firestore();
+
+    const ref: CollectionReference = firestore.collection("fans");
+    if (status === 'offline') {
+        ref.where("uid", "==", uid).limit(1).get()
+            .then((querySnapshot) => {
                 const docSnapshot: QueryDocumentSnapshot = querySnapshot.docs[0];
                 if (docSnapshot === null) {
                     // return null;
-                    throw Error("User presence document not found");
+                    throw Error("Fan not found");
                 }
-
-                return docSnapshot.id;
-            }).then((docId) => {            
+                const docId = docSnapshot.id;
                 return ref.doc(docId).set({ online: false, lastSeen: moment().utc().valueOf() }, { merge: true }).then((wr: WriteResult) => {
                     return {
                         docId: docId, writeResult: wr
                     } as unknown as Pair<String, WriteResult>;
                 });
-            }).then((res: Pair<String, WriteResult>) => {    
+            })
+            .then((res: Pair<String, WriteResult>) => {
                 console.log(`Updated firestore presence doc ${res.first} at ${res.second.writeTime}`);
                 return true;
             })
@@ -44,15 +50,6 @@ export const onUserStatusChanged = functions.database.ref("/status/{userId}")
                 console.log(`Failed to update online status. ${err}`)
                 return false;
             });
-
-            // let querySnapshot = await ref.where("uid", "==", uid).limit(1).get();
-            // let docSnapshot: QueryDocumentSnapshot = querySnapshot.docs[0];
-            // if (docSnapshot == null) {
-            //     return false;
-            // }
-
-            // let docId = docSnapshot.id;
-            // await ref.doc(docId).set({ online: false, lastSeen: moment().utc().valueOf() }, { merge: true });                
-        }
-        return false;
-    });
+    }
+    return false;
+}
